636d17445403a94d4f0cfe55e266875ff9a5ab53
Navong <bongchannavong68@gmail.com>
2025-01-25 05:01:36 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-import Home from '@/components/Home'
 
-const page = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
+import { Suspense } from 'react';
+import LoadMoreNewsList from '@/components/LoadMoreLists';
+import { fetchNews } from '../lib/fetchNews';
+import Header from '@/components/Header';
+import SkeletonLoader from '@/components/SkeletonLoader';
+import { Article } from '@/type/article';
+
+export const revalidate = 60;
+
+const ITEMS_PER_PAGE = 6;
+
+export default async function Home() {
+    const newsPromise = fetchNews('general');
+
+    return (
+        <main className="p-6">
+            <Header category={"Top Headlines"} />
+            <Suspense fallback={<SkeletonLoader />}>
+                {/* Wrap LoadMoreNewsList in a dynamic wrapper to allow client-side rendering */}
+                <NewsWrapper newsPromise={newsPromise} />
+            </Suspense>
+        </main>
+    );
 }
 
-export default page
+type NewsPromise = Promise<{ articles: Article[] }>;
+
+async function NewsWrapper({ newsPromise }: { newsPromise: NewsPromise }) {
+    const news = await newsPromise;
+    const filteredArticles = news.articles.filter(article => article.urlToImage !== null);
+    return <LoadMoreNewsList articles={filteredArticles} itemsPerPage={ITEMS_PER_PAGE} />;
+}
