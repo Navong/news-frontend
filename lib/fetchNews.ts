9afa1086ed09240e0fc697b71795274d3f6aaeeb
Navong <bongchannavong68@gmail.com>
2025-01-25 03:17:41 +0900

    change contents security policy

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -2,16 +2,24 @@ import { Article } from "@/type/article";
 
 export async function fetchNews(category: string) {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-    const res = await fetch(
-        `${baseUrl}/api/news/${category}`
-    );
-
-    if (!res.ok) throw new Error("Failed to fetch news.");
-    return res.json();
+    try {
+        const res = await fetch(`${baseUrl}/api/news/${category}`);
+        if (!res.ok) throw new Error("Failed to fetch news.");
+        return res.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 export async function fetchArticleById(id: string, category: string) {
-    const news = await fetchNews(category);
-    const article = news.articles.find((article: Article) => article.url === id);
-    return article || null;
+    try {
+        const news = await fetchNews(category);
+        const article = news.articles.find((article: Article) => article.url === id);
+        return article || null;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
+
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
